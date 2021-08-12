8b195ad49e42f4f0befc5c38003f6a0468b8090c
Junan Kim <junankim18@naver.com>
2021-08-12 20:01:29 +0900

    에러고쳤습니다. 꼭 개인 브랜치 파서 작업해주세요!

--- testapp/urls.py
+++ testapp/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup'),
-    path('find/', find, name = "find"), 
-    path('accept/<int:pk>', accept, name = "accept"), 
-    path('reject/<int:pk>', reject, name = "reject"), 
-    path('mypage/', mypage, name = "mypage"), 
-    path('follow/<int:pk>', follow, name = "follow"), 
+    path('find/', find, name="find"),
+    path('accept/<int:pk>', accept, name="accept"),
+    path('reject/<int:pk>', reject, name="reject"),
+    path('mypage/', mypage, name="mypage"),
+    path('follow/<int:pk>', follow, name="follow"),
     path('find_card', find_card, name="find_card"),
     path('get/<int:pk>', get, name="get"),
-    path('page/<int:my_id>/<int:other_id>', page, name = "page"),
-    path('trade/<int:pk>', trade, name = "trade"),
-    path('tradepage/', tradepage, name = "tradepage"),
-   # 나의 id와 상대방의 id를 동시에 전달함 
+    path('page/<int:my_id>/<int:other_id>', page, name="page"),
+    path('trade/<int:pk>', trade, name="trade"),
+    path('tradepage/', tradepage, name="tradepage"),
+    # 나의 id와 상대방의 id를 동시에 전달함
 
 ]
